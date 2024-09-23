69677aa4ae790050329c1a7e7d9bc8b1f4a21101
Abhishek <abhishek.raman018@gmai.com>
2024-09-23 15:22:10 +0530

    added: prompt bar and toolbar

--- src/pages/flowPage.tsx
+++ src/pages/flowPage.tsx
@@ -22,10 +22,37 @@ const flowPage = ( { sessionCookie } : any) => {
 
     return(
         <>
-            <div className="w-full h-full flex flex-col items-center justify-center">
-              <p className="font-bold text-3xl">What do you wanna create ?</p>
-              <input type="text" placeholder="A flying car" className="md:w-64" />
-            </div>
+
+        {/* Very cool prompt bar */}
+
+
+            {/* <div className="w-screen h-screen flex flex-col justify-items-center items-center gap-12 justify-center">
+              <p className="font-bold text-5xl">What do you want to create ?</p>
+              <input type="text" placeholder="A flying car" className=" w-1/2 p-3 border-purple-400 border-4 rounded-lg" />
+            </div> */}
+<div className="flex flex-row items-center  w-screen  h-screen ">
+              
+        <div className="h-screen flex flex-col items-center justify-center max-w-48 border-r-3 rounded-lg border-gray-700 shadow-lg self-start">
+              <div className="flex flex-col gap-6 items-center p-8 w-full">
+                  <p>
+                    Tool 1
+                  </p>
+                  <p>
+                    Tool 2
+                  </p>
+                  <p>
+                    Tool 3
+                  </p>
+              </div>
+        </div>
+
+        <div className="h-screen min-w-48 justify-center self-center ml-32 align-middle items-center">
+              <div className="h-32 w-24 absolute -z-12 p-12 border rounded-lg border-gray-700 shadow-lg bg-gray-400 justify-center items-center">
+                <p> Tool 1</p>
+              </div>
+        </div>
+</div>
+
         </>
     )
 

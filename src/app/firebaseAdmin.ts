3188fe645a18abe85daca3030e8a242c5f284d52
Diya Sundeep <diyasundeep64@gmail.com>
2024-11-02 12:57:06 +0530

    updating firebase admin

--- src/app/firebaseAdmin.ts
+++ src/app/firebaseAdmin.ts
@@ -1,25 +1,26 @@
-import { initializeApp, applicationDefault, getApps } from 'firebase-admin/app';
+import { initializeApp, applicationDefault, getApps } from "firebase-admin/app";
 
-var admin = require("firebase-admin")
-var serviceAccount = require("../../craft-9429a-firebase-adminsdk-5jobn-30946753c4.json")
+var admin = require("firebase-admin");
 
 //Thanks to this guy
 //https://github.com/firebase/firebase-admin-node/issues/2111#issuecomment-1636441596
 
 const usedApps = getApps();
-const adminAppConfig ={
+const adminAppConfig = {
   credential: admin.credential.cert({
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
     // The private key needs to be replaced with newlines
-    privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
-    
-  })
-}
+    privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(
+      /\\n/g,
+      "\n"
+    ),
+  }),
+};
 
-const adminApp = usedApps.length === 0
-? initializeApp(adminAppConfig,"admin-app")
-: usedApps[0]
+const adminApp =
+  usedApps.length === 0
+    ? initializeApp(adminAppConfig, "admin-app")
+    : usedApps[0];
 
-
-export default adminApp
+export default adminApp;
